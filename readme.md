c5a75749e3c51321f8daa79c6649e0d0551c73b1
joeyfortuna <joey_fortuna@ziffdavis.com>
2020-07-05 15:19:41 -0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+## Scavenge
+### QRCode-based Scavenger Hunt
+
+All the web code can just be accessed here: https://s3.amazonaws.com/natterbot.com/qrcode/index.html
+
+To create the codes, build a JSON file as an array, with one object per QRCode.
+
+Properties of the objects should be: 
+
+* file_name - this is just to create a temporary single-code image
+* clue_location - this will be readable text on the clue so you can tell where to hide it
+* msg - this is the clue itself -- a hint leading to the next clue!
+
+One high-level object should also be in the array:
+
+* master_file - this is the name of the image with all of the clues packed together
+
+To create the printable page full of clues, run "process_clues.py" in the same directory as your json file (note that the file name should end with "_scvg.json").
---
+++ web/qrCodeScanner.js
@@ -0,0 +1,66 @@
+const myqrcode = window.qrcode;
+
+const video = document.createElement("video");
+const canvasElement = document.getElementById("qr-canvas");
+const canvas = canvasElement.getContext("2d");
+
+const qrResult = document.getElementById("qr-result");
+const outputData = document.getElementById("outputData");
+const btnScanQR = document.getElementById("btn-scan-qr");
+
+let scanning = false;
+let initialized = false;
+
+myqrcode.callback = res => {
+  if (res) {
+    outputData.innerText = res;
+    scanning = false;
+    document.getElementById('jinglePlayer').play();
+
+    video.srcObject.getTracks().forEach(track => {
+      track.stop();
+    });
+
+    qrResult.hidden = false;
+    canvasElement.hidden = true;
+    btnScanQR.hidden = false;
+  }
+};
+
+btnScanQR.onclick = () => {
+
+  btnScanQR.classList.add("wobbler");
+  if (!initialized) {
+    initialized = true;
+    document.getElementById('jinglePlayer').play();
+  }
+  navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "environment" } })
+    .then(function(stream) {
+      scanning = true;
+      qrResult.hidden = true;
+      btnScanQR.hidden = true;
+      canvasElement.hidden = false;
+      video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
+      video.srcObject = stream;
+      video.play();
+      tick();
+      scan();
+    });
+};
+
+function tick() {
+  canvasElement.height = video.videoHeight;
+  canvasElement.width = video.videoWidth;
+  canvas.drawImage(video, 0, 0, canvasElement.width, canvasElement.height);
+
+  scanning && requestAnimationFrame(tick);
+}
+
+function scan() {
+  try {
+    myqrcode.decode();
+  } catch (e) {
+    setTimeout(scan, 300);
+  }
+}
